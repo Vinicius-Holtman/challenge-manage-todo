bc24bf0341b5087206f491223f8870d11da5f9ee
Vinicius <viniholtman28@gmail.com>
2022-11-09 11:36:46 -0300

    Created function to check user exists

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const usernameExist = users.find(user => user.username === username)
+
+  if (!usernameExist) {
+    return response.status(404).json({ error: 'Username not found!' })
+  }
+
+  request.user = usernameExist;
+  return next()
 }
 
 app.post('/users', (request, response) => {
