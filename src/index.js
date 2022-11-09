d6d86e38d8e7c95291c8759bc2ebc428b7307cd1
Vinicius <viniholtman28@gmail.com>
2022-11-09 11:38:01 -0300

    Created function to create new todo

--- src/index.js
+++ src/index.js
@@ -51,7 +51,18 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const newTodo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(newTodo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
